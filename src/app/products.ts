c1dc7b37952b3ffeffa8e5630073d4b976e4b571
GinnetteJCH <ginnettejch@gmail.com>
2019-08-20 10:47:48 -0600

    Add Table

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,16 +1,36 @@
 export const products = [
   {
-    name: 'Phone XL',
+    name: 'Frappuccino Caramelo',
     price: 799,
     description: 'A large phone with one of the best screens'
   },
   {
-    name: 'Phone Mini',
+    name: 'Strawberry Frappuccino ',
     price: 699,
     description: 'A great phone with one of the best cameras'
   },
   {
-    name: 'Phone Standard',
+    name: 'Sausage, Cheddar & Egg Breakfast Sandwich',
+    price: 299,
+    description: ''
+  },
+  {
+    name: 'Frappuccino vainilla',
+    price: 299,
+    description: ''
+  },
+  {
+    name: 'Java Ship Frappuccino ',
+    price: 299,
+    description: ''
+  },
+  {
+    name: 'Caramel Ribbon Crunch Frappuccino',
+    price: 299,
+    description: ''
+  },
+  {
+    name: 'Skinny Vainilla Latte',
     price: 299,
     description: ''
   }
